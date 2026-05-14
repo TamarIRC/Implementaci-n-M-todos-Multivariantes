f88d2440a4c55290c1a66e2226fb28dce9d1d343
Tamarl <tamar.rojas@usach.cm>
2026-05-14 19:47:19 -0400

    Primeras Lineas de código sobre normalidad multivariante

--- Código Practico.R
+++ Código Practico.R
@@ -1,3 +1,12 @@
 library(readxl)
 Base_Multi <- read_excel("Base Multi.xlsx")
 View(Base_Multi)
+
+#Inferencia Multivariada----
+##Test de normalidad multivariada----
+library(MVN)
+#de manera global, mardia si acepta normalidad
+mvn(data = Base_Multi[, 1:5], mvnTest = "mardia", univariateTest = "SW") 
+mvn(data = Base_Multi[, 1:5], mvnTest = "mardia", univariateTest = "Lillie") #Se rechaza de manera univariada EDAD
+mvn(data = Base_Multi[, 1:5], mvnTest = "mardia", univariateTest = "AD")  #Se rechaza de manera univariada EDAD
+mvn(data = Base_Multi[, 1:5], mvnTest = "mardia", univariateTest = "CVM") #Se rechaza de manera univariada EDAD
