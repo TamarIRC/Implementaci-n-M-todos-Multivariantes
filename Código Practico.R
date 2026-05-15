76f0a7497b145f599c7f3e1f881fb4469baafcda
Tamarl <tamar.rojas@usach.cm>
2026-05-14 20:07:57 -0400

    Cambio de Libreria moderna

--- Código Practico.R
+++ Código Practico.R
@@ -1,12 +1,14 @@
-library(readxl)
-Base_Multi <- read_excel("Base Multi.xlsx")
+
+
 View(Base_Multi)
 
 #Inferencia Multivariada----
 ##Test de normalidad multivariada----
 library(MVN)
-#de manera global, mardia si acepta normalidad
-mvn(data = Base_Multi[, 1:5], mvnTest = "mardia", univariateTest = "SW") 
-mvn(data = Base_Multi[, 1:5], mvnTest = "mardia", univariateTest = "Lillie") #Se rechaza de manera univariada EDAD
-mvn(data = Base_Multi[, 1:5], mvnTest = "mardia", univariateTest = "AD")  #Se rechaza de manera univariada EDAD
-mvn(data = Base_Multi[, 1:5], mvnTest = "mardia", univariateTest = "CVM") #Se rechaza de manera univariada EDAD
+
+mvn(data = Base_Multi[, 1:5], mvn_test = "mardia", univariate_test = "SW")
+mvn(data = Base_Multi[, 1:5], mvn_test = "mardia", univariate_test = "Lillie") #Se rechaza de manera univariada EDAD
+mvn(data = Base_Multi[, 1:5], mvn_test = "mardia", univariate_test = "AD") #Se rechaza de manera univariada EDAD
+mvn(data = Base_Multi[, 1:5], mvn_test = "mardia", univariate_test = "CVM") #Se rechaza de manera univariada EDAD
+
+
