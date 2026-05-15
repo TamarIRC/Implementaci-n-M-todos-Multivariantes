32a291cfe89b782d218da7e62b80ea920c77f9f9
Tamarl <tamar.rojas@usach.cm>
2026-05-14 20:52:06 -0400

    Se terminó M-Box para la parte de inferencia univariada y se agregó su respectiva explicación el Markdown

--- Código Practico.R
+++ Código Practico.R
@@ -23,3 +23,27 @@ vec <- Base_Multi[, 1:5]
 Sexo <- Base_Multi$Sexo
 # Ejecutamos el test comparando Hombres vs Mujeres con todos los datos
 HotellingsT2Test(vec[Sexo == "HOMBRE", ], vec[Sexo == "MUJER", ])
+
+##K (3) muestras aleatorias homogéneas e independientes(Comparación de medias MANOVA)----
+
+#Creamos el modelo MANOVA
+modelo_manova <- manova(cbind(Edad, Peso, IMC, Presión_Arterial_Sistólica) ~ Estado_Nutricional, data = Base_Multi)
+
+summary(modelo_manova, test = "Wilks") #Este es el que normalmente en el curso de utilizaría 
+summary(modelo_manova, test = "Pillai") #Dice que es uno más riguroso que Wilks
+summary(modelo_manova, test = "Hotelling-Lawley")
+summary(modelo_manova, test = "Roy")
+
+
+##Homogeneidad de matrices de varianza-covarianza (M de Box)----
+
+#install.packages("biotools")
+library(biotools)
+
+#Para grupos por sexo:
+boxM(vec, Base_Multi$Sexo)
+#Para grupos por estado nutricional:
+boxM(vec, Base_Multi$Estado_Nutricional)
+
+
+#REDUCCIÓN DE LA DIMENSIONALIDAD----
