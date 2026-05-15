260c594a51902821cf960699d7f93344b4ae7f2e
Tamarl <tamar.rojas@usach.cm>
2026-05-14 20:15:21 -0400

    Dos Muestras

--- Código Practico.R
+++ Código Practico.R
@@ -12,3 +12,14 @@ mvn(data = Base_Multi[, 1:5], mvn_test = "mardia", univariate_test = "AD") #Se r
 mvn(data = Base_Multi[, 1:5], mvn_test = "mardia", univariate_test = "CVM") #Se rechaza de manera univariada EDAD
 
 
+##Dos muestras aleatorias homogéneas e independientes (Comparación de medias)----
+
+#install.packages("DescTools")
+library(DescTools)
+
+# Separamos solo las variables numéricas (columnas 1 a 5) en "vec"
+vec <- Base_Multi[, 1:5]
+# Guardamos la variable Sexo para el filtro
+Sexo <- Base_Multi$Sexo
+# Ejecutamos el test comparando Hombres vs Mujeres con todos los datos
+HotellingsT2Test(vec[Sexo == "HOMBRE", ], vec[Sexo == "MUJER", ])
